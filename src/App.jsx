913ff2c964bb85218720c292e7ec4ce2bf9dc945
George Papagapitos <george.g.papagapitos@gmail.com>
2021-07-15 15:13:49 -0500

    set up tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import Account from "./components/Account"
+import { QueryClient, QueryClientProvider } from "react-query";
+import Account from "./components/Account";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Account transferToPerson="Jimmy John" />
-    </>
-  )
-}
+    </QueryClientProvider>
+  );
+};
